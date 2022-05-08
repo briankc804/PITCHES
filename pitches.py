7b55e4509dba59e00b99b1802257151b626bc34d
briankc804 <briankc804@gmail.com>
2022-05-08 23:48:48 +0300

    created a route for registering a user

--- pitches.py
+++ pitches.py
@@ -28,7 +28,12 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='about')
-    
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm
+    return render_template('register.html', title='login',form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
