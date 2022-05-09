bdd4f93d52d23fd8c192de3e25d32dae2e2e63fc
briankc804 <briankc804@gmail.com>
2022-05-09 03:31:15 +0300

    added login and register code on the template files

--- pitches.py
+++ pitches.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,url_for
+from crypt import methods
+from flask import Flask,render_template,url_for,flash,redirect
 from forms import RegistrationForm,LoginForm
 app =Flask(__name__)
 
@@ -30,15 +31,18 @@ def about():
     return render_template('about.html',title='about')
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST'])
 def register():
-    form = RegistrationForm
-    return render_template('register.html', title='register',form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Your account {form.username.data} created!','successs')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register',form=form)
 
 @app.route("/login")
 def login():
-    form = LoginForm
-    return render_template('login.html', title='login',form=form)
+    form = LoginForm()
+    return render_template('login.html', title='Login',form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
