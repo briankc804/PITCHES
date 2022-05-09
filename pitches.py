b1edaf22f0d0484647f18b3d3a66a62f69572e48
briankc804 <briankc804@gmail.com>
2022-05-09 14:28:36 +0300

    created a post class

--- pitches.py
+++ pitches.py
@@ -15,7 +15,12 @@ class User(db.model):
     username = db.Column(db.String(100),unique=True,nullable=False)
     image_file = db.Collumn(db.String(28),nullable=False,default='default.jpg')
     password = db.Column(db.String(35),nullable=False)
-    
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.model):
+    id = db.column(db.Integer,primary_key=True)       
 
 posts = [
     {
