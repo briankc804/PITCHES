eed3e6b2bd6eec0f3d5c979c386bf7d37eb4c7a5
briankc804 <briankc804@gmail.com>
2022-05-09 13:58:15 +0300

    downloaded sqlalchemy

--- pitches.py
+++ pitches.py
@@ -1,5 +1,6 @@
 from crypt import methods
 from flask import Flask,render_template,url_for,flash,redirect
+
 from forms import RegistrationForm,LoginForm
 app =Flask(__name__)
 
@@ -39,9 +40,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register',form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'george@pitches.com' and form.password.data == '12345':
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('login unsuccessful.please check email and password','danger')
     return render_template('login.html', title='Login',form=form)
 
 if __name__=='__main__':
