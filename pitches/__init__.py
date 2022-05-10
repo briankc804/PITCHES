f68cb458ab68c1244830ca8b47cd7896f6d29a17
briankc804 <briankc804@gmail.com>
2022-05-11 00:18:00 +0300

    downloded and imported bcrypt into our files

--- pitches/__init__.py
+++ pitches/__init__.py
@@ -1,10 +1,12 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '75b26b88cde7aa12e91b21a2b3310311c37bbfb9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from pitches import routes
--- pitches/forms.py
+++ pitches/forms.py
@@ -10,7 +10,9 @@ class RegistrationForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+               validators=[DataRequired(), EqualTo('password')])
+
+
     submit = SubmitField('Sign Up')
 
 
