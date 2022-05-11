247ac2ae45768dc253cba42ab334f4a507962718
briankc804 <briankc804@gmail.com>
2022-05-11 23:14:08 +0300

    imported login manager and created a syntax to login a user when he creates an account

--- pitches/__init__.py
+++ pitches/__init__.py
@@ -2,13 +2,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import login_manager
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '75b26b88cde7aa12e91b21a2b3310311c37bbfb9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = login_manager(app)
+login_manager = LoginManager(app)
 
 from pitches import routes
