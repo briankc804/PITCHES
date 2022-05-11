2dfb0fa95cc7d1ffadf184ea240dbd66aa6dcc5d
briankc804 <briankc804@gmail.com>
2022-05-12 01:09:12 +0300

    update changes

--- pitches/routes.py
+++ pitches/routes.py
@@ -1,8 +1,8 @@
-from flask import  render_template, url_for, flash, redirect
+from flask import  render_template, url_for, flash, redirect, request
 from pitches import app, db , bcrypt
 from pitches.forms import RegistrationForm, LoginForm
 from pitches.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 
@@ -57,7 +57,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password,form.password.data):
             login_user(user,remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -70,6 +71,7 @@ def logout():
     return redirect(url_for('home'))
 
 @app.route("/account")
+@login_required
 def account():
     return render_template('account.html', title='Account')
 
